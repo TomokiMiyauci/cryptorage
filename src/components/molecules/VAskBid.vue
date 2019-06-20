b0e2ac817f74b87fb676a1498d108b555e8b974f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-06-20 22:07:09 +0900

    feat: create presentation components

---
+++ src/components/molecules/VAskBid.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-data-table :items="items" :headers="headers" loading hide-actions>
+    <template #items="{item}">
+      <td>{{ item.ticker }}</td>
+      <td>{{ item.ask }}</td>
+      <td>{{ item.spread }}</td>
+      <td>{{ item.bid }}</td>
+    </template>
+  </v-data-table>
+</template>
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      headers: [
+        { text: 'Ticker', value: 'ticker' },
+        { text: 'Ask', value: 'ask' },
+        { text: 'Spread', value: 'spread' },
+        { text: 'Bid', value: 'bid' }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/molecules/VOrderBook.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-data-table :headers="header" :items="items" loading hide-actions>
+    <template #items="{item}">
+      <td class="text-xs-center">{{ item.askAmount }}</td>
+      <td class="text-xs-center">{{ item.rate }}</td>
+      <td class="text-xs-center">{{ item.bidAmount }}</td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      header: [
+        {
+          text: 'Ask',
+          value: 'askAmount',
+          align: 'center',
+          sortable: false
+        },
+        { text: 'Rate', value: 'rate', align: 'center', sortable: false },
+        {
+          text: 'Bid',
+          value: 'bidAmount',
+          align: 'center',
+          sortable: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
