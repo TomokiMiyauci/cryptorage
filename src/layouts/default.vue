925a7090693a9ee5e1073339d919b2c22f938ff4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-06-30 20:12:53 +0900

    fix(default.vue): delete templates directry

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,28 @@
 <template>
-  <the-default />
+  <v-app>
+    <the-toolber @switchDrawer="drawer = !drawer" />
+    <the-navber :drawer="drawer" />
+    <the-app />
+    <the-footer />
+  </v-app>
 </template>
 
 <script>
-import TheDefault from '~/layouts/templates/TheDefault'
+import TheToolber from '~/components/organisms/TheToolber'
+import TheNavber from '~/components/organisms/TheNavber'
+import TheFooter from '~/components/organisms/Thefooter'
+import TheApp from '~/components/organisms/TheApp'
 export default {
   components: {
-    TheDefault
+    TheToolber,
+    TheNavber,
+    TheFooter,
+    TheApp
+  },
+  data() {
+    return {
+      drawer: false
+    }
   }
 }
 </script>
